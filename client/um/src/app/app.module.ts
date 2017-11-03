0d6e1ff80e35ff45c7a3cf261674f4faa8b32bb1
Nizami <nizami.dev@gmail.com>
2017-11-03 19:16:36 +0300

    Added angular material

--- client/um/src/app/app.module.ts
+++ client/um/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDModule } from './modules/md.module';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MainComponent } from './main/main.component';
     MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDModule
   ],
   providers: [],
   bootstrap: [AppComponent]
